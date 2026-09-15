431b7026c826f34c1e0d795fb2bccda4507898f5
jamalanas760-ship-it <jamalanas760@gmail.com>
2026-09-15 09:45:04 +0300

    fix(tables): restore modern tables scope

--- src/components/manage/TablesManagerModern.tsx
+++ src/components/manage/TablesManagerModern.tsx
@@ -1,5 +1,9 @@
 import { TablesManagerApproved } from "./TablesManagerApproved";
 
 export function TablesManagerModern({ restaurantId }: { restaurantId: string }) {
-  return <TablesManagerApproved restaurantId={restaurantId} />;
+  return (
+    <div className="qs-tables-modern">
+      <TablesManagerApproved restaurantId={restaurantId} />
+    </div>
+  );
 }
