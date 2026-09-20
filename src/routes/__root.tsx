ce1d7dfa823dec4a0a84f3e589682af8a00a6dfc
jamalanas760-ship-it <jamalanas760@gmail.com>
2026-09-20 11:05:10 +0300

    Design: apply rebrand across the application

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -14,6 +14,7 @@ import masterCss from "../master-design.css?url";
 import uxCss from "../ux-refinement.css?url";
 import modernCss from "../modern-pass.css?url";
 import approvedCss from "../approved-design.css?url";
+import rebrandCss from "../rebrand-2026.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { I18nProvider } from "@/lib/i18n";
 import { Toaster } from "@/components/ui/sonner";
@@ -124,6 +125,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { rel: "stylesheet", href: uxCss },
       { rel: "stylesheet", href: modernCss },
       { rel: "stylesheet", href: approvedCss },
+      { rel: "stylesheet", href: rebrandCss },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Sora:wght@400;600;700&family=Manrope:wght@400;500;600;700&family=IBM+Plex+Sans+Arabic:wght@400;500;600;700&display=swap" },
